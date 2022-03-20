4571567946dde57a4d046410af955d233263d56e
Michael Kohler <me@michaelkohler.info>
2022-03-20 20:02:50 +0100

    Change title

--- app/root.tsx
+++ app/root.tsx
@@ -18,7 +18,7 @@ export const links: LinksFunction = () => {
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "Remix Notes",
+  title: "where.directory",
   viewport: "width=device-width,initial-scale=1",
 });
 
