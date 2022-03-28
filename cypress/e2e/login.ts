351ec88c3c1d00864a06d19f8b394e8f563ec4f5
Michael Kohler <me@michaelkohler.info>
2022-03-28 22:31:58 +0200

    Extend login test

--- cypress/e2e/login.ts
+++ cypress/e2e/login.ts
@@ -13,14 +13,20 @@ describe("login tests", () => {
     cy.then(() => ({ email: loginForm.email })).as("user");
 
     cy.visit("/");
-    cy.findByRole("link", { name: /sign up/i }).click();
 
+    cy.findByRole("link", { name: /sign up/i }).click();
     cy.findByRole("textbox", { name: /email/i }).type(loginForm.email);
     cy.findByLabelText(/password/i).type(loginForm.password);
     cy.findByRole("button", { name: /create account/i }).click();
 
     cy.findByRole("link", { name: /dashboard/i }).click();
+    cy.findByText(/no trip selected/i);
+
     cy.findByRole("button", { name: /logout/i }).click();
-    cy.findByRole("link", { name: /log in/i });
+
+    cy.findByRole("link", { name: /log in/i }).click();
+    cy.findByRole("textbox", { name: /email/i }).type(loginForm.email);
+    cy.findByLabelText(/password/i).type(loginForm.password);
+    cy.findByRole("button", { name: /log in/i }).click();
   });
 });
