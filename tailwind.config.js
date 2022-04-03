ae8d9cb99177cfbf2e0b2ac66b719eedb4f09b0f
Michael Kohler <me@michaelkohler.info>
2022-04-03 12:04:51 +0200

    Add initial version of homepage (fixes #16)

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,9 @@ module.exports = {
         sans: ["Raleway", ...defaultTheme.fontFamily.sans],
         title: ["Dosis", ...defaultTheme.fontFamily.sans],
       },
+      height: {
+        128: "32rem",
+      },
     },
   },
   plugins: [],
