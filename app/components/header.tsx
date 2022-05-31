02f96f5c94900c0afca89fc2d47100aa3fe2e513
Michael Kohler <me@michaelkohler.info>
2022-05-31 18:58:45 +0200

    Add mobile nav

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -4,67 +4,140 @@ import { useOptionalUser } from "~/utils";
 
 export default function Header() {
   const user = useOptionalUser();
+  const [mobileMenuOpen, setMobileMenuOpen] = React.useState(!user);
+
   return (
-    <header className="flex items-center justify-between bg-slate-800 p-4 text-white">
-      <section className="flex flex-col md:flex-row">
+    <header className="bg-slate-800 p-4 text-white">
+      <div className="flex w-full flex-row justify-between">
         <h1 className="text-3xl font-bold">
           <Link to="/">
             where<span className="text-sky-200">.directory</span>
           </Link>
         </h1>
-        <nav className="mt-3 flex flex-row md:mt-0 md:ml-20">
-          {user && (
-            <>
-              <Link
-                to="/trips"
-                className="text-white-700 flex items-center justify-center border-b-2 border-slate-800 py-2 pl-0 pr-8 text-base font-semibold hover:border-white md:pl-8 md:pr-8"
-              >
-                Trips
-              </Link>
-              <Link
-                to={`/${user.username}`}
-                className="text-white-700 flex items-center justify-center border-b-2 border-slate-800 py-2 pr-8 text-base font-semibold hover:border-white md:pl-8 md:pr-8"
-              >
-                Profile
-              </Link>
-              <Link
-                to="/account"
-                className="text-white-700 flex items-center justify-center border-b-2 border-slate-800 py-2 pr-8 text-base font-semibold hover:border-white md:pl-8 md:pr-8"
-              >
-                Account
-              </Link>
-            </>
-          )}
-        </nav>
-      </section>
 
-      <section>
-        {user ? (
-          <Form action="/logout" method="post">
-            <button
-              type="submit"
-              className="text-white-100 rounded bg-slate-600 py-2 px-4 hover:bg-slate-500 active:bg-slate-500"
-            >
-              Logout
-            </button>
-          </Form>
-        ) : (
-          <div className="flex flex-row space-x-4">
-            <Link
-              to="/join"
-              className="text-white-100 flex items-center justify-center rounded bg-yellow-600 py-2 px-4 font-medium hover:bg-yellow-500 active:bg-yellow-500"
-            >
-              Sign up
-            </Link>
-            <Link
-              to="/login"
-              className="flex items-center justify-center rounded bg-slate-600 px-4 py-2 font-medium text-white hover:bg-slate-500 active:bg-slate-500"
-            >
-              Log In
-            </Link>
-          </div>
-        )}
-      </section>
+        {/* DESKTOP NAV */}
+        <section className="hidden w-full items-center justify-between lg:flex">
+          <nav className="mt-3 mt-0 ml-20 flex flex-row">
+            {user && (
+              <>
+                <Link
+                  to="/trips"
+                  className="text-white-700 flex items-center justify-center border-b-2 border-slate-800 py-2 px-8 text-base font-semibold hover:border-white"
+                >
+                  Trips
+                </Link>
+                <Link
+                  to={`/${user.username}`}
+                  className="text-white-700 flex items-center justify-center border-b-2 border-slate-800 py-2 px-8 text-base font-semibold hover:border-white"
+                >
+                  Profile
+                </Link>
+                <Link
+                  to="/account"
+                  className="text-white-700 flex items-center justify-center border-b-2 border-slate-800 py-2 px-8 text-base font-semibold hover:border-white"
+                >
+                  Account
+                </Link>
+              </>
+            )}
+          </nav>
+
+          <section>
+            {user ? (
+              <Form action="/logout" method="post">
+                <button
+                  type="submit"
+                  className="text-white-100 rounded bg-slate-600 py-2 px-4 hover:bg-slate-500 active:bg-slate-500"
+                >
+                  Logout
+                </button>
+              </Form>
+            ) : (
+              <div className="flex flex-row space-x-4">
+                <Link
+                  to="/join"
+                  className="text-white-100 flex items-center justify-center rounded bg-yellow-600 py-2 px-4 font-medium hover:bg-yellow-500 active:bg-yellow-500"
+                >
+                  Sign up
+                </Link>
+                <Link
+                  to="/login"
+                  className="flex items-center justify-center rounded bg-slate-600 px-4 py-2 font-medium text-white hover:bg-slate-500 active:bg-slate-500"
+                >
+                  Log In
+                </Link>
+              </div>
+            )}
+          </section>
+        </section>
+
+        <div>
+          <button
+            onClick={() => setMobileMenuOpen(!mobileMenuOpen)}
+            className="text-white-100 rounded bg-slate-600 py-2 px-4 hover:bg-slate-500 active:bg-slate-500 lg:hidden"
+          >
+            &equiv;
+          </button>
+        </div>
+      </div>
+
+      {/* MOBILE NAV */}
+      {mobileMenuOpen && (
+        <section className="flex w-full flex-col lg:hidden">
+          <nav className="mt-3 flex flex-col">
+            {user && (
+              <>
+                <Link
+                  to="/trips"
+                  className="text-white-700 flex items-center justify-center py-4 pl-0 text-base font-semibold"
+                >
+                  Trips
+                </Link>
+                <Link
+                  to={`/${user.username}`}
+                  className="text-white-700 flex items-center justify-center py-4 text-base font-semibold"
+                >
+                  Profile
+                </Link>
+                <Link
+                  to="/account"
+                  className="text-white-700 flex items-center justify-center py-4 text-base font-semibold"
+                >
+                  Account
+                </Link>
+              </>
+            )}
+          </nav>
+
+          <section className="mt-6 flex items-center justify-center">
+            {user ? (
+              <Form action="/logout" method="post">
+                <button
+                  type="submit"
+                  className="text-white-100 rounded bg-slate-600 py-2 px-4 hover:bg-slate-500 active:bg-slate-500"
+                >
+                  Logout
+                </button>
+              </Form>
+            ) : (
+              <div className="flex flex-row space-x-4">
+                <Link
+                  to="/join"
+                  className="text-white-100 flex items-center justify-center rounded bg-yellow-600 py-2 px-4 font-medium hover:bg-yellow-500 active:bg-yellow-500"
+                >
+                  Sign up
+                </Link>
+                <Link
+                  to="/login"
+                  className="flex items-center justify-center rounded bg-slate-600 px-4 py-2 font-medium text-white hover:bg-slate-500 active:bg-slate-500"
+                >
+                  Log In
+                </Link>
+              </div>
+            )}
+          </section>
+        </section>
+      )}
     </header>
   );
 }
