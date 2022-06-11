a91b4265d929555351fe05f23eeae945d85cb6a2
Michael Kohler <me@michaelkohler.info>
2022-06-11 18:46:17 +0200

    Some code migrations due to upgrades

--- app/utils.test.ts
+++ app/utils.test.ts
@@ -1,13 +1,13 @@
 import { validateEmail } from "./utils";
 
 test("validateEmail returns false for non-emails", () => {
-  expect(validateEmail(undefined)).toBe(false);
-  expect(validateEmail(null)).toBe(false);
-  expect(validateEmail("")).toBe(false);
-  expect(validateEmail("not-an-email")).toBe(false);
-  expect(validateEmail("n@")).toBe(false);
+  expect(validateEmail(undefined)).equal(false);
+  expect(validateEmail(null)).equal(false);
+  expect(validateEmail("")).equal(false);
+  expect(validateEmail("not-an-email")).equal(false);
+  expect(validateEmail("n@")).equal(false);
 });
 
 test("validateEmail returns true for emails", () => {
-  expect(validateEmail("kody@example.com")).toBe(true);
+  expect(validateEmail("kody@example.com")).equal(true);
 });
---
+++ cypress.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  e2e: {
+    baseUrl: "http://localhost:8811",
+    specPattern: "cypress/e2e/**/*.ts",
+  },
+  viewportWidth: 1400,
+  viewportHeight: 800,
+});
