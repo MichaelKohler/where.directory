8cd8c00233b6477a66413222eb2b7f56d2c454b7
Michael Kohler <me@michaelkohler.info>
2023-08-20 12:52:52 +0200

    Further adjustments to prepare for Remix v2

--- remix.config.js
+++ remix.config.js
@@ -5,8 +5,11 @@ module.exports = {
   cacheDirectory: "./node_modules/.cache/remix",
   ignoredRouteFiles: ["**/.*", "**/*.css", "**/*.test.{js,jsx,ts,tsx}"],
   future: {
+    v2_dev: true,
     v2_errorBoundary: true,
     v2_meta: true,
     v2_normalizeFormMethod: true,
+    v2_headers: true,
   },
+  serverModuleFormat: "cjs",
 };
